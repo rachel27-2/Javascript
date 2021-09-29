ea1d4b880ea51061b311f0aaf6a13a871a405dff
rachel27-2 <rachelkendal127@gmail.com>
2021-09-29 11:15:37 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hello world!");
+
+const msg = "this is a message";
+
+console.log(msg);
+
+console.log("Rachel");
+console.log("Kendal");
+console.log("London");
+console.log("pisces");
+
+console.log(`%c` + msg, `color: orange; background-color: black; font-family: fantasy; padding: 10px; font-weight: bold;`)
